d5f6fe72ffd5d4b7f23fda63e33a70d55db80c7a
zhonghang986 <631182531@qq.com>
2016-08-03 10:36:34 +0800

    Git第二次改变

--- ViewController.m
+++ ViewController.m
@@ -19,6 +19,7 @@
     BXTextField *bxtext = [[BXTextField alloc] initWithFrame:CGRectMake(50, 100, 200, 30)];
     bxtext.placeholder = @"请输入身份证号";
     NSLog(@"Git客户端发生改变");
+    NSLog(@"Git客户端发生改变第二次");
     bxtext.layer.borderWidth = 1;
     bxtext.layer.borderColor = [UIColor blackColor].CGColor;
     bxtext.font = [UIFont systemFontOfSize:14.0];
